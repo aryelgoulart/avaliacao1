0c184b55cd74ebbd9f2ed6e9b498ec0caf989adb
aryelgoulart <aryelgoulart@gmail.com>
2016-10-03 21:39:41 -0300

    primeiro commit

---
+++ ja.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+window.onload = function() {
+var divTres = document.getElementById("demo3");
+divTres.onclick = function() {
+if (this.className == "cordiv") {
+	this.className = "";
+	} else {
+	this.className = "cordiv"
+	}
+} 
+}
+</script>
